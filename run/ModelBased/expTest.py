d0b8021c915304d59570a974c26b7333e0bded5c
Guillaume Viejo <guillaume.viejo@gmail.com>
2013-05-30 23:34:14 +0200

    New class for model based

---
+++ run/ModelBased/expTest.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+# encoding: utf-8
+"""
+expGraph.py
+
+script to test the modelbased
+
+Copyright (c) 2013 Guillaume VIEJO. All rights reserved.
+"""
+
+import sys
+import os
+from optparse import OptionParser
+import numpy as np
+from pylab import plot, figure, show, subplot, legend, xlim
+
+sys.path.append("../../src")
+from fonctions import *
+from ColorAssociationTasks import CATS
+from GraphConstruction import ModelBased
+
+
+
+# -----------------------------------
+# ARGUMENT MANAGER
+# -----------------------------------
+#if not sys.argv[1:]:
+#    sys.stdout.write("Sorry: you must specify at least 1 argument")
+#    sys.stdout.write("More help avalaible with -h or --help option")
+#    sys.exit(0)
+parser = OptionParser()
+parser.add_option("-i", "--input", action="store", help="The name of the directory to load", default=False)
+(options, args) = parser.parse_args() 
+# -----------------------------------
+
+# -----------------------------------
+# FONCTIONS
+# -----------------------------------    
+def iterationStep(iteration, values, display = True):
+    
+    #display current stimulus
+    state = cats.getStimulus(iteration)
+
+    #choose best action 
+    action = getBestActionSoftMax(state, values, beta)
+    
+    # set response + get outcome
+    reward = cats.getOutcome(state, action, iteration)
+    answer.append((reward==1)*1)
+
+    # model based
+    #mods.addTransition((state, action))
+
+    # QLearning
+    delta = reward + gamma*np.max(values[0][values[state]]) - values[0][values[(state,action)]]
+    values[0][values[(state, action)]] = values[0][values[(state, action)]] + alpha*delta
+
+
+
+    if display == True:
+        print (state, action, reward)
+        print cats.correct
+        displayQValues(cats.states, cats.actions, values)
+        print '\n'
+# -----------------------------------
+
+
+# -----------------------------------
+# PARAMETERS + INITIALIZATION
+# -----------------------------------
+gamma = 0.1 #discount factor
+alpha = 1
+beta = 3
+
+nb_trials = 42
+
+cats = CATS()
+mods = ModelBased(cats.states, cats.actions)
+values = createQValuesDict(cats.states, cats.actions)
+Qdata = []
+
+# -----------------------------------
+# Learning session
+# -----------------------------------
+answer = []
+cats.reinitialize(nb_trials, 'meg')
+for j in xrange(nb_trials):
+    iterationStep(j, values, False)
+# -----------------------------------
+
+# -----------------------------------
+# Plot
+# -----------------------------------
+Qdata = np.array(Qdata)
+
+mods.display()
+# -----------------------------------
+
+
+
+
+
+
+
--- src/GraphConstruction.py
+++ src/GraphConstruction.py
@@ -13,12 +13,35 @@ sys.path.append("../../src")
 import os
 import numpy as np
 from fonctions import *
+import networkx as nx
+import matplotlib.pyplot as plt
 
-class GraphConstruction():
+class ModelBased():
     """Class that implement a trees construction based on
     Color Association Experiments from Brovelli & al 2011
     """
     
-    def __init__(state, action):
+    def __init__(self, state, action):
         self.state = state
         self.action = action
+        self.g = nx.Graph()
+        self.g.add_nodes_from(state)
+        for i in range(3):
+            self.g.add_nodes_from(action)
+        self.g.add_edges_from(self.computeEdgesBunch(state, action))
+        
+    def reinitialize():
+        self.__init__(self.state, self.action)
+
+    def computeEdgesBunch(self, state, action):
+        tmp = []
+        for i in state:
+            for j in action:
+                tmp.append((i, j,{'weight':1.0/len(action)}))
+        return tmp
+
+    def display(self):        
+        nx.draw(self.g)
+        plt.show()
+
+    #def addTransition
