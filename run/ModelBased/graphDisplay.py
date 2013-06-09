b838128029c568c1bdb7d41210e65487ba758510
Guillaume Viejo <guillaume.viejo@gmail.com>
2013-06-10 00:08:14 +0200

    work on graph display

---
+++ run/ModelBased/graphDisplay.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+# encoding: utf-8
+"""
+expGraph.py
+
+script to test the modelbased
+
+Copyright (c) 2013 Guillaume VIEJO. All rights reserved.
+"""
+
+import sys
+import os
+from optparse import OptionParser
+import numpy as np
+from pylab import *
+from matplotlib import *
+sys.path.append("../../src")
+from fonctions import *
+from ColorAssociationTasks import CATS
+from GraphConstruction import ModelBased
+from Plot import PlotTree
+
+
+# -----------------------------------
+# ARGUMENT MANAGER
+# -----------------------------------
+#if not sys.argv[1:]:
+#    sys.stdout.write("Sorry: you must specify at least 1 argument")
+#    sys.stdout.write("More help avalaible with -h or --help option")
+#    sys.exit(0)
+parser = OptionParser()
+parser.add_option("-i", "--input", action="store", help="The name of the directory to load", default=False)
+(options, args) = parser.parse_args() 
+# -----------------------------------
+
+# -----------------------------------
+# FONCTIONS
+# -----------------------------------
+def iterationStep(iteration, mods, display = True):
+    print iteration
+    state = cats.getStimulus(iteration)
+
+    #choose best action from model based
+    action = mods.chooseAction(mods.g[state])
+
+    # set response + get outcome
+    reward = cats.getOutcome(state, action)
+    answer.append((reward==1)*1)
+
+    # update Model Based
+    mods.updateTrees(state, reward)
+
+    if display == True:
+        print (state, action, reward)
+        print_dict(mods.g[state])
+        print cats.correct
+        print '\n'
+# -----------------------------------
+
+
+# -----------------------------------
+# PARAMETERS + INITIALIZATION
+# -----------------------------------
+gamma = 0.1 #discount factor
+alpha = 1
+beta = 3
+
+nb_trials = 42
+nb_blocs = 100
+cats = CATS()
+mods = ModelBased(cats.states, cats.actions)
+tree = PlotTree()
+
+# -----------------------------------
+# Learning session
+# -----------------------------------
+cats.reinitialize(nb_trials, 'meg')
+mods.reinitialize(cats.states, cats.actions)
+answer = []
+for j in xrange(nb_trials):
+    iterationStep(j, mods, False)
+
+# -----------------------------------
+
+# -----------------------------------
+# Plot
+# -----------------------------------
+
+# -----------------------------------
+
+
+
+
+
+
+
---
+++ src/Plot.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python
+# encoding: utf-8
+"""
+Plot.py
+
+Use matplotlib
+Copyright (c) 2013 Guillaume VIEJO. All rights reserved.
+"""
+
+import sys
+sys.path.append("../../src")
+import os
+import numpy as np
+from fonctions import *
+import matplotlib.pyplot as plt
+
+class PlotCATS():
+    """Class that allow to plot in different way results
+    from the color association task ."""
+
+    def __init__(self, data):
+        self.data = data
+
+    def plotPerformanceAll(self):
+        tmp = []
+        for i in self.data.iterkeys():
+            for j in self.data[i].iterkeys():
+                tmp.append(self.data[i][j]['sar'][0:42,2])
+        mean = np.mean(tmp, 0)
+        var = np.var(tmp, 0)
+        plot(mean, label = 'human performance')
+        fill_between(range(len(mean)), mean-var/2, mean+var/2, facecolor = 'green', interpolate = True, alpha = 0.1)
+        legend()
+        grid()
+        show()
+
+    def plotComparison(self, dic_data):
+        tmp = []
+        for i in self.data.iterkeys():
+            for j in self.data[i].iterkeys():
+                tmp.append(self.data[i][j]['sar'][0:42,2])
+        mean = np.mean(tmp, 0)
+        var = np.var(tmp, 0)        
+        plot(mean,'o-', label = 'human performance')
+        fill_between(range(len(mean)), mean-var/2, mean+var/2, facecolor = 'green', interpolate = True, alpha = 0.1)
+
+        for i in dic_data.iterkeys():
+            mean2 = np.mean(dic_data[i], 0)
+            var2 = np.var(dic_data[i], 0)
+            plot(mean2,'o-', label = i+' performance')
+            fill_between(range(len(mean2)), mean2-var2/2, mean2+var2/2, facecolor = 'green', interpolate = True, alpha = 0.1)
+        legend()
+        grid()
+
+        show()
+        
+
+
+class PlotTree():
+    """Class to plot trees ."""
+
+    def __init__(self):
+        self.decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+        self.leafNode = dict(boxstyle="round4", fc="0.8")
+        self.arrow_args = dict(arrowstyle="<-")
+
+    def getNumberLeafs(self, tree):
+        numLeafs = 0
+        for k in tree.iterkeys():
+            if type(tree[k]).__name__=='dict':
+                if len(tree[k].keys()) <> 0:
+                    numLeafs += self.getNumberLeafs(tree[k])
+                else:
+                    numLeafs += 1                                             
+        return numLeafs
+
+    def getTreeDepth(self, tree):
+        depth = []
+        for k in tree.iterkeys():
+            if type(tree[k]).__name__=='dict':
+                if len(tree[k].keys()) <> 0:
+                    depth.append(1+self.getTreeDepth(tree[k]))
+                else:
+                    depth.append(1)
+        return np.max(depth)
+                    
+
+    def plotNode(self, nodeTxt, centerPt, parentPt, nodeType):
+        self.ax1.annotate(nodeTxt,
+                          xy=parentPt,
+                          xycoords='axes fraction',
+                          xytext=centerPt,
+                          textcoords='axes fraction',
+                          va="center",
+                          ha="center",
+                          bbox=nodeType,
+                          arrowprops=self.arrow_args)
+
+    def plotMidText(self, cntrPt, parentPt, txtString):
+        xMid = (parentPt[0]-cntrPt[0])/2.0 + cntrPt[0]
+        yMid = (parentPt[1]-cntrPt[1])/2.0 + cntrPt[1]
+        self.ax1.text(xMid, yMid, txtString)
+
+    def createPlot(self, tree):
+        fig = plt.figure(1, facecolor='white')
+        fig.clf()
+        axprops = dict(xticks=[], yticks=[])
+        self.ax1 = plt.subplot(111, frameon=False, **axprops)
+        self.totalW = float(self.getNumberLeafs(tree))
+        self.totalD = float(self.getTreeDepth(tree))
+        self.xOff = -0.5/self.totalW
+        self.yOff = 1.0;
+        self.plotTree(tree, [0.0,1.0], (0.5,1.0), 'origin')
+        plt.show()
+
+    def plotTree(self, tree, parent, nodeTxt):
+        nnodes = 0
+        for k in tree.iterkeys():
+            if type(tree[k]).__name__=='dict':
+                nnodes += 1
+        
+                
+
+
+
+    def plotTree2(self, myTree, parentPt, nodeTxt):
+        numLeafs = self.getNumberLeafs(myTree)
+        self.getTreeDepth(myTree)
+        firstStr = myTree.keys()[0]
+        cntrPt = (self.xOff + (1.0 + float(numLeafs))/2.0/self.totalW,\
+                  self.yOff)
+        self.plotMidText(cntrPt, parentPt, nodeTxt)
+        self.plotNode(firstStr, cntrPt, parentPt, self.decisionNode)
+        secondDict = myTree[firstStr]
+        self.yOff = self.yOff - 1.0/self.totalD
+        for key in secondDict.keys():
+            if type(secondDict[key]).__name__=='dict':
+                self.plotTree(secondDict[key],cntrPt,str(key))
+            else:
+                self.xOff = self.xOff + 1.0/self.totalW
+                self.plotNode(secondDict[key], (self.xOff, self.yOff),cntrPt, self.leafNode)
+                self.plotMidText((self.xOff, self.yOff), cntrPt, str(key))
+        self.yOff = self.yOff + 1.0/self.totalD
+            
+    
+                                                        
+        
+    def retrieveTree(i):
+        listOfTrees =[{'no surfacing': {0: 'no', 1: {'flippers': \
+                                                     {0: 'no', 1: 'yes'}}}},
+                      {'no surfacing': {0: 'no', 1: {'flippers': \
+                                                     {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}
+                      ]
+        return listOfTrees[i]
+
+
