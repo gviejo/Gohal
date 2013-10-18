5a7d430a6974d176e95288ef17d5d208586a8f78
Guillaume <guillaume.viejo@gmail.com>
2013-10-18 19:09:24 +0200

    work on subject optimization

--- run/Optimization/extractBestParameters.py
+++ run/Optimization/extractBestParameters.py
@@ -33,6 +33,7 @@ if not sys.argv[1:]:
 parser = OptionParser()
 parser.add_option("-i", "--input", action="store", help="The name of the directory to load", default=False)
 parser.add_option("-m", "--model", action="store", help="The name of the model to test", default=False)
+parser.add_option("-o", "--output", action="store", help="The name of the output files", default=False)
 
 (options, args) = parser.parse_args() 
 # -----------------------------------
@@ -40,46 +41,12 @@ parser.add_option("-m", "--model", action="store", help="The name of the model t
 # -----------------------------------
 # FONCTIONS
 # -----------------------------------
-def testModel():    
-    for i in xrange(nb_blocs):
-        sys.stdout.write("\r Blocs : %i" % i); sys.stdout.flush()         
-        cats.reinitialize()
-        model.initialize()
-        for j in xrange(nb_trials):
-            state = cats.getStimulus(j)
-            action = model.chooseAction(state)
-            reward = cats.getOutcome(state, action)
-            model.updateValue(reward)
-# -----------------------------------
 
-# -----------------------------------
-# HUMAN LEARNING
-# -----------------------------------
-human = HLearning(dict({'meg':('../../PEPS_GoHaL/Beh_Model/',42), 'fmri':('../../fMRI',39)}))
 # -----------------------------------
 
 # -----------------------------------
 # PARAMETERS + INITIALIZATION
 # -----------------------------------
-eta = 0.0001     # variance of evolution noise v
-var_obs = 0.05   # variance of observation noise n
-gamma = 0.630     # discount factor
-init_cov = 10   # initialisation of covariance matrice
-kappa = 0.1      # unscentered transform parameters
-beta = 1.6666   
-noise = 0.01
-length_memory = 8
-threshold = 1.2
-
-nb_trials = human.responses['meg'].shape[1]
-#nb_blocs = human.responses['meg'].shape[0]
-nb_blocs = 40
-cats = CATS()
-
-models = dict({'kalman':KalmanQLearning('kalman', cats.states, cats.actions, gamma, beta, eta, var_obs, init_cov, kappa),
-               'bwm':BayesianWorkingMemory('bwm', cats.states, cats.actions, length_memory, noise, threshold)})
-
-model = models[options.model]
 
 # -----------------------------------
 
@@ -88,12 +55,13 @@ model = models[options.model]
 # -----------------------------------
 f = open(options.input, 'rb')
 p = pickle.load(f)
+f.close()
 # -----------------------------------
 
 # -----------------------------------
-#order data
+# Order data
 # -----------------------------------
-data = dict()
+
 n_search = p['search']
 subject = p['subject']
 n_parameters = len(p['p_order'])
@@ -114,42 +82,25 @@ elif fname == 'fmin':
 else:
     print "scipy function not specified\n"
     sys.exit()
-# -----------------------------------
 
 
 # -----------------------------------
-# Plot
+
 # -----------------------------------
-params = {'backend':'pdf',
-          'axes.labelsize':10,
-          'text.fontsize':10,
-          'legend.fontsize':10,
-          'xtick.labelsize':8,
-          'ytick.labelsize':8,
-          'text.usetex':False}          
+# Saving 
+# -----------------------------------
+output = open(options.output, 'w')
+output.write("# Optimization function : "+fname+"\n")
+output.write("# Nb search : "+str(n_search)+"\n")
 
-if fname == 'minimize':
-    fig = figure(figsize = (9,4))
-    ax = fig.add_subplot(111, projection='3d')
-    for i in xrange(len(X)):
-        c = np.random.rand(3,)
-        ax.scatter(X[i,:,0], X[i,:,1], fun[0], marker='o', color = c)
-        ax.set_xlabel(p['p_order'][0])
-        ax.set_ylabel(p['p_order'][1])
+for i in xrange(len(p['subject'])):
+    best = X[i][fun[i] == np.max(fun[i])]
+    l = round(np.max(fun[i]), 2)
+     
+    line = p['subject'][i]+":(likelihood:"+str(l)+","
+    for j in xrange(len(p['p_order'])):
+        line = line+p['p_order'][j]+":"+str(round(best[0][j], 2))+","    
+    line = line[0:-1] + ")\n"
+    output.write(line)
 
-figure()
-if n_parameters == 2:
-  for i in xrange(len(X)):
-    c = np.random.rand(3,)
-    plot(X[i,:,0], X[i,:,1], 'o', color = c, markersize = i+3)
-  grid()
-elif n_parameters == 3:
-  ax = fig.add_subplot(111, projection='3d')
-  for i in xrange(len(X)):
-    c = np.random.rand(3,)
-    ax.scatter(X[i,:,0], X[i,:,1], X[i,:,2], marker ='o', alpha = 1, color = c)
-  
-
-
-show()
-
+output.close()    
